7fa834e019eee7d79955d65a3a1abed6c75c98e1
Celien Nanson <cesliens@gmail.com>
2017-07-19 22:18:52 +0200

    Remove redundant information within Lane class

--- src/Lane.h
+++ src/Lane.h
@@ -10,8 +10,6 @@ class Lane {
 		int id; // 0 is right, 1 is middle and 2 is right
 		vector<double> x_wp;
 		vector<double> y_wp;
-		vector<double> d_wp;
-		vector<double> s_wp;
 
 		// Destructor
 		virtual ~Lane();
@@ -20,8 +18,8 @@ class Lane {
 		Lane(int id)
 			:id(id){} 
 
-		Lane(int id, vector<double> x_wp, vector<double> y_wp, vector<double> d_wp, vector<double> s_wp)
-			: id(id), x_wp(x_wp), y_wp(y_wp), d_wp(d_wp), s_wp(s_wp){}
+		Lane(int id, vector<double> x_wp, vector<double> y_wp)
+			: id(id), x_wp(x_wp), y_wp(y_wp){}
 }; 
 
 #endif /* LANE_H_ */
