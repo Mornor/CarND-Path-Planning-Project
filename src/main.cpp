fcb7246a28ee4b7ef663795af240acdcd0109a39
Celien Nanson <cesliens@gmail.com>
2017-08-13 10:11:57 -0500

    Smooth out the trajectory. Bug in spline.

--- src/main.cpp
+++ src/main.cpp
@@ -9,6 +9,8 @@
 #include "Eigen-3.3/Eigen/QR"
 #include "json.hpp"
 
+#include "spline.h"
+
 using namespace std;
 
 // for convenience
@@ -183,7 +185,13 @@ int main() {
 		map_waypoints_dy.push_back(d_y);
 	}
 
-	h.onMessage([&map_waypoints_x,&map_waypoints_y,&map_waypoints_s,&map_waypoints_dx,&map_waypoints_dy](uWS::WebSocket<uWS::SERVER> ws, char *data, size_t length, uWS::OpCode opCode) {
+	// Start in middle lane (0 being the left one, and 2 the right one) 
+	int lane = 1; 
+
+	// Target velocity (mph)
+	double ref_vel = 49.5; 
+
+	h.onMessage([&map_waypoints_x,&map_waypoints_y,&map_waypoints_s,&map_waypoints_dx,&map_waypoints_dy,lane,ref_vel](uWS::WebSocket<uWS::SERVER> ws, char *data, size_t length, uWS::OpCode opCode) {
 	// "42" at the start of the message means there's a websocket message event.
 	// The 4 signifies a websocket message
 	// The 2 signifies a websocket event
@@ -219,12 +227,117 @@ int main() {
 				// Sensor Fusion Data, a list of all other cars on the same side of the road.
 				auto sensor_fusion = j[1]["sensor_fusion"];
 
+				int prev_size = previous_path_x.size(); 
+
 				json msgJson;
+
+				// 30m spaced waypoints. Will be interpolated with spline and fill with more waypoints.
+				vector<double> ptsx;
+				vector<double> ptsy; 
+
+				// Keep track of the reference state.
+				double ref_x = car_x;
+				double ref_y = car_y; 
+				double ref_yaw = deg2rad(car_yaw);
+
+				// If the prev_path is empty, use the current car space
+				if(prev_size < 2){
+					// Define 2 points that make a path tangent to the car
+					double prev_car_x = car_x - cos(car_yaw);
+					double prev_car_y = car_y - sin(car_yaw); 
+
+					ptsx.push_back(prev_car_x);
+					ptsx.push_back(car_x); 
+					ptsy.push_back(prev_car_y);
+					ptsy.push_back(car_y);
+
+				// If there is a previous path, use it as a start referecne 	
+				}else{
+					// Also define a tangent, using the previous path
+					// Get the last place of where the car is, compute it's heading at this time 
+					ref_x = previous_path_x[prev_size - 1];
+					ref_y = previous_path_y[prev_size - 1]; 
+
+					double ref_x_prev = previous_path_x[prev_size - 2];
+					double ref_y_prev = previous_path_y[prev_size - 2];
+					ref_yaw = atan2(ref_y - ref_y_prev, ref_x - ref_x_prev); 
+
+					// Use 2 points that makes the path tangent to the previous path end point
+					ptsx.push_back(ref_x_prev);
+					ptsx.push_back(ref_x); 
+					ptsy.push_back(ref_y_prev);
+					ptsy.push_back(ref_y); 
+				}
+
+				// In Frenet coordinates, add evely spaced (30m) points ahead of the starting reference. 
+				vector<double> next_wp0 = getXY(car_s + 30, (2 + 4 * lane), map_waypoints_s, map_waypoints_x, map_waypoints_y);
+				vector<double> next_wp1 = getXY(car_s + 60, (2 + 4 * lane), map_waypoints_s, map_waypoints_x, map_waypoints_y);
+				vector<double> next_wp2 = getXY(car_s + 90, (2 + 4 * lane), map_waypoints_s, map_waypoints_x, map_waypoints_y);
+
+				ptsx.push_back(next_wp0[0]);
+				ptsx.push_back(next_wp1[0]);
+				ptsx.push_back(next_wp2[0]);
+
+				ptsy.push_back(next_wp0[1]);
+				ptsy.push_back(next_wp1[1]); 
+				ptsy.push_back(next_wp2[2]);
+				// So now, the ptsx and ptsy have 5 points : 2 previous points and the wpx, which are 30, 60 and 90m from the reference starting point
+
+				// Transformation (shift in rotation): Make sure the last point of the previous path is at the origin (0,0), and its angle is at 0 degres. 
+				// Basically, take the car's refeerence frame at the starting reference points. 
+				// Make the math much easier after. 
+				for(int i = 0; i < ptsx.size(); i++){
+					double shift_x = ptsx[i] - ref_x; 
+					double shift_y = ptsy[i] - ref_y; 
+					ptsx[i] = (shift_x * cos(0 - ref_yaw) - shift_y * sin(0 - ref_yaw)); 
+					ptsy[i] = (shift_x * sin(0 - ref_yaw) + shift_y * cos(0 - ref_yaw)); 
+				}
+
+				// Create a spline and set (x,y) to it
+				tk::spline s; 
+				s.set_points(ptsx, ptsy); 
 
 				vector<double> next_x_vals;
 				vector<double> next_y_vals;
 
+				// Populate the (x,y) points that the simulator will be using with the previous path from the last time
+				// Instead of re-creating all the path from scratch, just add points to it. 
+				for(int i = 0; i < previous_path_x.size(); i++){
+					next_x_vals.push_back(previous_path_x[i]);
+					next_y_vals.push_back(previous_path_y[i]);  
+				}
+
+				// Break up spline points to travel at the desire speed
+				double target_x = 30.0; // Define an horizon 
+				double target_y = s(target_x); // Get the y for the given x coordinates using the spline function. 
+				double target_dist = sqrt((target_x) * (target_x) + (target_y) * (target_y)); 
+				double x_add_on = 0; // We start at the origin using the shift transformation above. 
+
+				// Adding on the points along the spline for the simulator 
+				for(int i = 1; i <= 50 - previous_path_x.size(); i++){
+					double N = (target_dist / (.02 * ref_vel / 2.24)); // 2.24 because we want m/s instead of miles/h.
+					double x_point = x_add_on + (target_x) / N;
+					double y_point = s(x_point); 
+
+					x_add_on = x_point; 
+
+					double x_ref = x_point; 
+					double y_ref = y_point; 
+
+					// Get back to normal coordinates (after the shifting above)
+					// Local to global coordinates
+					x_point = (x_ref * cos(ref_yaw) - y_ref * sin(ref_yaw));
+					y_point = (x_ref * sin(ref_yaw) + y_ref * cos(ref_yaw)); 
+
+					x_point += ref_x; 
+					y_point += ref_y;  
+
+					next_x_vals.push_back(x_point); 
+					next_y_vals.push_back(y_point); 
+				}
+
 				// TODO: define a path made up of (x,y) points that the car will visit sequentially every .02 seconds
+				/***
 				double dist_inc = 0.5;
 				for(int i = 0; i < 50; i++){
 					double next_s = car_s + (i + 1) * dist_inc; 
@@ -233,6 +346,7 @@ int main() {
 					next_x_vals.push_back(xy[0]);
 					next_y_vals.push_back(xy[1]);
 				}
+				***/
 
 				msgJson["next_x"] = next_x_vals;
 				msgJson["next_y"] = next_y_vals;
